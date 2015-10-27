3bda5c44c05b10c227e85d32f02988f661a8840f
Danilo F. Silva <danilofes@gmail.com>
2015-10-27 20:24:50 -0200

    Corrigi a bug do retangulo.

--- poo-paint/src/principal/FerramentaRetangulo.java
+++ poo-paint/src/principal/FerramentaRetangulo.java
@@ -1,5 +1,6 @@
 package principal;
 
+import figuras.Elipse;
 import figuras.Figura;
 import figuras.Retangulo;
 
@@ -11,7 +12,11 @@ public class FerramentaRetangulo extends Ferramenta {
 
 	@Override
 	public Figura criaFigura(int x0, int y0, int x1, int y1) {
-		return new Retangulo(x0, y0, x1 - x0, y1 - y0);
+		int xc = Math.min(x0, x1);
+		int yc = Math.min(y0, y1);
+		int largura = Math.abs(x1 - x0);
+		int altura = Math.abs(y1 - y0);
+		return new Retangulo(xc, yc, largura, altura);
 	}
 
 	@Override
